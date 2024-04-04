5f0db4e8419944c09cd16fe049d742c9895bd63c
Roman Kukhalashvili <cyberdna12@gmail.com>
2024-04-04 12:13:35 +0400

    chore: added font

--- src/pages/SuggestionsPage.jsx
+++ src/pages/SuggestionsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function SuggestionsPage() {
-    
 
     return (
         <>
-            <h1>Suggestions page</h1>
+            <div>
+                
+            </div>
         </>
     )
 }
