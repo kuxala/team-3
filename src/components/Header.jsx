9b2e41b3a1efa3dd9ba5d7c2c6f3ff6ca4bbf25b
ani <aniabaishvili1@gmail.com>
2024-04-05 19:43:01 +0400

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function HeaderComponent(){
+    return(
+        <Header>Hello</Header>
+    )
+}
+const Header = styled.div`
+    color:red;
+`
